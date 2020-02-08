925ceadbc12268d9cb6ab8fe87433112995c7a27
unknown <monmaramba@yahoo.com>
2020-02-08 09:48:18 -0800

    initial commit

---
+++ server/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.DATABASE);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// Models
+const { User } = require("./models/user");
+
+//==================
+//users
+//==================
+// user register
+app.post("/api/users/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    res.status(200).json({
+      success: true,
+      userdata: doc
+    });
+  });
+});
+// user login
+app.post('/api/user/login', (req, res) => {
+  // find the email
+    User.findOne({'email':req.body.email}, (err, user)=> {
+      if(!user) return res.json({loginSuccess: false, message:"auth failed, email not found"});
+    })
+  // check password match
+  user.comparePassword(req.body.password,(err,isMatch)=>{
+    if(!isMatch) return res.json({loginSuccess: false, message:'Wrong password'});
+
+    user.generateToken((err, ))
+  })
+
+  // generate token
+})
+
+const port = process.env.PORT || 3002;
+
+app.listen(port, () => {
+  console.log(`The Server is tuned in to ${port}`);
+});
