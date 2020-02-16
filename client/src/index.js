35699922162bb3499f361352be59243b3217e31b
unknown <monmaramba@yahoo.com>
2020-02-16 06:34:35 -0800

    fixes for server & console warnings. Style sass fix for header

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./routes";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
