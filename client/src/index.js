2ace278d282e970bf388eb1a1bce8fbd54ea21fd
unknown <monmaramba@yahoo.com>
2020-03-01 14:36:03 -0800

    code review. Renamed index in reducers to root-reducer

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import promiseMiddleware from "redux-promise";
 import ReduxThunk from "redux-thunk";
-import Reducer from "./redux/reducers";
+import Reducer from "./redux/reducers/root-reducer";
 
 import "./index.styles.scss";
 
