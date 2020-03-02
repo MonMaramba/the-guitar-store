20e0343de33871794266e08260f773a4f84660c6
unknown <monmaramba@yahoo.com>
2020-03-01 20:09:18 -0800

    user-dashboard and user-hoc components created for user/dashboard route

---
+++ client/src/components/user/user-dashboard.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import UserLayout from "../../hoc/user-hoc";
+
+const UserDashboard = () => {
+  return (
+    <UserLayout>
+      <div>Dashboard</div>;
+    </UserLayout>
+  );
+};
+
+export default UserDashboard;
--- client/src/routes.js
+++ client/src/routes.js
@@ -5,12 +5,15 @@ import Layout from "./hoc/layout.hoc";
 import RegisterLogin from "./components/register_login/register-login.component";
 import Register from "./components/register_login/register.component";
 
+import UserDashboard from "./components/user/user-dashboard.component";
+
 import HomePage from "./pages/home.component";
 
 const Routes = () => {
   return (
     <Layout>
       <Switch>
+        <Route path="/user/dashboard" exact component={UserDashboard} />
         <Route path="/" exact component={HomePage} />
         <Route path="/register_login" exact component={RegisterLogin} />
         <Route path="/register" exact component={Register} />
