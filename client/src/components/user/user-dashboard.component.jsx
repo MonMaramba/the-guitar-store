40bfc3c1500c295239a395e2e902990e2b33158b
unknown <monmaramba@yahoo.com>
2020-03-02 19:08:31 -0800

    more dashboard styling

--- client/src/components/user/user-dashboard.component.jsx
+++ client/src/components/user/user-dashboard.component.jsx
@@ -1,12 +1,31 @@
 import React from "react";
 import UserLayout from "../../hoc/user-hoc";
+import Mybutton from "../utils/button";
 
 import "./user-dashboard.styles.scss";
 
 const UserDashboard = () => {
   return (
     <UserLayout>
-      <div>Dashboard</div>
+      <div>
+        <div className="user_nfo_panel">
+          <h1>User information</h1>
+
+          <span>name</span>
+          <span>lastname</span>
+          <span>email</span>
+
+          <Mybutton
+            type="default"
+            title="Edit account info"
+            linkTo="/user/user_profile"
+          />
+        </div>
+        <div className="user_nfo_panel">
+          <h1>History of purchases</h1>
+          <div className="user_product_block_wrapper">history</div>
+        </div>
+      </div>
     </UserLayout>
   );
 };
