dff361f3967b2343579d6165ac05b2c625c632e1
unknown <monmaramba@yahoo.com>
2020-03-03 19:34:49 -0800

    private access for select routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -5,6 +5,7 @@ import Layout from "./hoc/layout.hoc";
 import RegisterLogin from "./components/register_login/register-login.component";
 import Register from "./components/register_login/register.component";
 
+import Auth from "./hoc/auth";
 import UserDashboard from "./components/user/user-dashboard.component";
 
 import HomePage from "./pages/home.component";
@@ -13,10 +14,18 @@ const Routes = () => {
   return (
     <Layout>
       <Switch>
-        <Route path="/user/dashboard" exact component={UserDashboard} />
-        <Route path="/" exact component={HomePage} />
-        <Route path="/register_login" exact component={RegisterLogin} />
-        <Route path="/register" exact component={Register} />
+        <Route
+          path="/user/dashboard"
+          exact
+          component={Auth(UserDashboard, true)}
+        />
+        <Route path="/" exact component={Auth(HomePage, null)} />
+        <Route
+          path="/register_login"
+          exact
+          component={Auth(RegisterLogin, false)}
+        />
+        <Route path="/register" exact component={Auth(Register, false)} />
       </Switch>
     </Layout>
   );
