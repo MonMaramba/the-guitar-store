1c5212a5a2942c42def216e88e2281d4a0ae44e0
unknown <monmaramba@yahoo.com>
2020-04-11 05:17:15 -0700

    add brand complete

--- client/src/components/user/Admin/manage_brands.component.jsx
+++ client/src/components/user/Admin/manage_brands.component.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import FormField from "../../utils/Form/formfield";
 
-import FormFields from "../../utils/Form/formfield";
 import {
   update,
   generateData,
   isFormValid,
   resetFields,
 } from "../../utils/Form/formActions";
-import { getBrands } from "../../../redux/actions/products_actions";
+import { getBrands, addBrand } from "../../../redux/actions/products_actions";
 
 import "./manage-brands.styles.scss";
 
@@ -44,7 +44,50 @@ class ManageBrands extends Component {
         ))
       : null;
 
+  updateForm = (element) => {
+    const newFormdata = update(element, this.state.formdata, "brands");
+    this.setState({
+      formError: false,
+      formdata: newFormdata,
+    });
+  };
+
+  resetFieldsHandler = () => {
+    const newFormData = resetFields(this.state.formdata, "brands");
+
+    this.setState({
+      formdata: newFormData,
+      formSuccess: true,
+    });
+  };
+
+  submitForm = (event) => {
+    event.preventDefault();
+
+    let dataToSubmit = generateData(this.state.formdata, "brands");
+    let formIsValid = isFormValid(this.state.formdata, "brands");
+    let existingBrands = this.props.products.brands;
+
+    if (formIsValid) {
+      this.props
+        .dispatch(addBrand(dataToSubmit, existingBrands))
+        .then((response) => {
+          if (response.payload.success) {
+            this.resetFieldsHandler();
+          } else {
+            this.setState({ formError: true });
+          }
+        });
+    } else {
+      this.setState({ formError: true });
+    }
+  };
+
   componentDidMount() {
+    this.props.dispatch(getBrands());
+  }
+
+  componentDidUpdate() {
     this.props.dispatch(getBrands());
   }
 
@@ -56,7 +99,21 @@ class ManageBrands extends Component {
           <div className="left">
             <div className="brands_container">{this.showCategoryItems()}</div>
           </div>
-          <div className="right">form</div>
+          <div className="right">
+            <form onSubmit={(event) => this.submitForm(event)}>
+              <FormField
+                id={"name"}
+                formdata={this.state.formdata.name}
+                change={(element) => this.updateForm(element)}
+              />
+              {this.state.formError ? (
+                <div className="error_label">Please check your data</div>
+              ) : null}{" "}
+              <button onClick={(event) => this.submitForm(event)}>
+                Add brand
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     );
--- client/src/redux/reducers/products_reducer.js
+++ client/src/redux/reducers/products_reducer.js
@@ -5,10 +5,10 @@ import {
   GET_WOODS,
   GET_PRODUCTS_TO_SHOP,
   ADD_PRODUCT,
-  CLEAR_PRODUCT
+  CLEAR_PRODUCT,
 } from "../actions/types";
 
-export default function(state = {}, action) {
+export default function (state = {}, action) {
   switch (action.type) {
     case GET_PRODUCTS_BY_SELL:
       return { ...state, bySell: action.payload };
@@ -16,13 +16,20 @@ export default function(state = {}, action) {
       return { ...state, byArrival: action.payload };
     case GET_BRANDS:
       return { ...state, brands: action.payload };
+
+    case ADD_PRODUCT:
+      return {
+        ...state,
+        addbrand: action.payload.success,
+        brands: action.payload.brands,
+      };
     case GET_WOODS:
       return { ...state, woods: action.payload };
     case GET_PRODUCTS_TO_SHOP:
       return {
         ...state,
         toShop: action.payload.articles,
-        toShopSize: action.payload.size
+        toShopSize: action.payload.size,
       };
     case ADD_PRODUCT:
       return { ...state, addProduct: action.payload };
